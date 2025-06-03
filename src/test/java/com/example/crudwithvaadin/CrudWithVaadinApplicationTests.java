db5a45d1efd222b4653305d4c46989c1bad28c39
Roberto Marquez <onebeartoe@gmail.com>
2025-06-02 20:46:35 -0500

    add original/inital tests

---
+++ src/test/java/com/example/crudwithvaadin/CrudWithVaadinApplicationTests.java
@@ -0,0 +1,31 @@
+package com.example.crudwithvaadin;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.BDDAssertions.*;
+import org.mockito.Mock;
+
+@SpringBootTest(classes = CrudWithVaadinApplication.class, webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+public class CrudWithVaadinApplicationTests {
+
+	@Autowired
+	private CustomerRepository repository;
+
+	@Autowired
+        DocumentRepository documentRepository;        
+//        @Mock 
+//        DocumentRepository documentRepository;
+        
+	@Test
+	public void shouldFillOutComponentsWithDataWhenTheApplicationIsStarted() {
+		then(this.repository.count()).isEqualTo(5);
+	}
+
+	@Test
+	public void shouldFindTwoBauerCustomers() {
+		then(this.repository.findByLastNameStartsWithIgnoreCase("Bauer")).hasSize(2);
+	}
+}
