ec3743a036e1dce51efb5a4940b8d3c69f6d0331
Roberto Marquez <onebeartoe@gmail.com>
2025-06-03 05:01:34 -0500

    add the initial document editor

--- src/main/java/com/example/crudwithvaadin/DocumentEditor.java
+++ src/main/java/com/example/crudwithvaadin/DocumentEditor.java
@@ -1,17 +1,19 @@
 
 package com.example.crudwithvaadin;
 
+import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.KeyNotifier;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.spring.annotation.SpringComponent;
 import com.vaadin.flow.spring.annotation.UIScope;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.util.StringUtils;
 
 /**
  *
@@ -20,6 +22,11 @@ import org.springframework.util.StringUtils;
 @UIScope
 public class DocumentEditor extends VerticalLayout implements KeyNotifier
 {
+    /**
+     * The currently edited customer
+     */
+    private Document document;    
+    
     DocumentRepository docRepo;
     
     final Grid<Document> grid;
@@ -29,26 +36,119 @@ public class DocumentEditor extends VerticalLayout implements KeyNotifier
     private final Button addNewBtn;
     
     VerticalLayout editor;
+    
+    TextField url = new TextField("URL");
+    
+    Button cancel = new Button("Cancel");
+    
+    Binder binder = new Binder<>(Document.class);
+    
+    private CustomerEditor.ChangeHandler changeHandler;
         
     @Autowired
     public DocumentEditor(DocumentRepository repo)
     {
         this.docRepo = repo;
-        this.editor = new VerticalLayout();
-        this.grid = new Grid<>(Document.class);
-        this.filterField = new TextField();
-        this.addNewBtn = new Button("New Document", VaadinIcon.PLUS.create());
-
-        // build layout
+                
+        this.filterField = new TextField();                
+        this.addNewBtn = new Button("New Document", VaadinIcon.PLUS.create());                
         HorizontalLayout actions = new HorizontalLayout(filterField, addNewBtn);
-
+        
+        this.grid = new Grid<>(Document.class);
+        
+        this.editor = new VerticalLayout();    
+        
+        editor.add(url);
+        
         add(actions, grid, editor);
+        
+        // Instantiate and edit new Customer the new button is clicked
+        addNewBtn.addClickListener(e -> editCustomer(new Document("", "", null)));
+//        addNewBtn.addClickListener(e -> editCustomer(new Document("", "", "")));
+
+
+	// bind using naming convention
+        binder.bindInstanceFields(this);                               
+       
+        setSpacing(true);
+        
+//!!TODO!!  - enable next 2 commented lines        
+//        save.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+//        delete.addThemeVariants(ButtonVariant.LUMO_ERROR);
+
+        addKeyPressListener(Key.ENTER, e -> save());
+
+//!!TODO!!  - enable next 3 commented lines                
+        // wire action buttons to save, delete and reset
+//        save.addClickListener(e -> save());
+//        delete.addClickListener(e -> delete());
+//        cancel.addClickListener(e -> editCustomer(customer));
+
+
+
+// This was false
+//        setVisible(false);                
+//        setVisible(false);        
+        
+        
     }
 
+    void save() {
+            docRepo.save(document);
+            changeHandler.onChange();
+    }    
+    
     void listDocuments(Customer customer) 
     {	
         
         grid.setItems(docRepo.findAllByCustomer(customer) );
         
+    }
+    
+//TODO: rename to editDocument!    
+public final void editCustomer(Document c) 
+        {            
+		if (c == null) {
+			setVisible(false);
+			return;
+		}
+	
+                final boolean persisted = c.getId() != null;
+		
+                if (persisted) {
+			// Find fresh entity for editing
+			// In a more complex app, you might want to load
+			// the entity/DTO with lazy loaded relations for editing
+			document = docRepo.findById(c.getId()).get();
+                        
+//                        documentEditor.listDocuments(customer);
+		}
+		else {
+			document = c;
+		}
+		
+                cancel.setVisible(persisted);                                                         
+                
+		// Bind customer properties to similarly named fields
+		// Could also use annotation or "manual binding" or programmatically
+		// moving values from fields to entities before saving
+		binder.setBean(document);
+         
+                
+                // this field was showing ',' as part of the number
+                var idValue = document.getId() == null ? "" : document.getId().toString();
+//                id.setValue( idValue );
+      
+		setVisible(true);                
+
+		// Focus first name initially
+		url.focus();
+	}    
+
+    public void setChangeHandler(CustomerEditor.ChangeHandler h) 
+    {
+        // ChangeHandler is notified when either save or delete
+        // is clicked
+        changeHandler = h;
     }
 }
